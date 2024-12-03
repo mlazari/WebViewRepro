7e2adfcf8e341cd8086c2f5436ac13b71b8a4056
Mihai Lazari <l3m3a3@gmail.com>
2024-12-03 10:45:15 +0200

    test issue 3619 (injectJavaScript method)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { useCallback, useRef, useState } from "react";
+import { Button, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function IndexScreen() {
+  const webView = useRef<WebView>(null);
+
+  const injectJavascript = useCallback(() => {
+    webView.current?.injectJavaScript("setTimeout(() => alert('Test Webview'), 1000)");
+  }, []);
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={{ flex: 1 }}
     >
-      <Text>Hello</Text>
+      <WebView
+        ref={webView}
+        style={{ flex: 1 }}
+        source={{ html: '<html><head></head><body></body></html>' }}
+      />
+      <View style={{ padding: 20 }}>
+        <Button title="Inject javascript" onPress={injectJavascript} />
+      </View>
     </View>
   );
 }
